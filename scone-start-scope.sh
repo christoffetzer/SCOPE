c220e232e4a09f453b95399cb58651e2baf0d73c
Christof Fetzer <christof.fetzer@gmail.com>
2017-07-22 23:34:36 +0200

    fixed ++ issue

--- scone-start-scope.sh
+++ scone-start-scope.sh
@@ -69,7 +69,7 @@ function deploy_stack {
     for NODENO in `seq 1 $no` ; do
         verbose  "Adding config for node $NODENO "
         envsubst < $dir/scope-template.yml >> $outputfile
-        ((PORT++))
+        ((++PORT))
     done
 
     # now start SCOPE
@@ -135,8 +135,8 @@ function  start_tunnel {
     for NODENO in `seq 1 $no` ; do
         verbose  "Adding tunnel for node $NODENO (local port $local_port)"
         ssh -O forward -S $controlpath -L  $local_port:$manager:$PORT $manager
-        ((local_port++))
-        ((PORT++))
+        ((++local_port))
+        ((++PORT))
     done
 
 }
