c80323223ab0232b01a8376571fe008d609557c0
Christof Fetzer <christof.fetzer@gmail.com>
2017-08-02 14:42:21 +0200

    moved to a Makefile based CI testing

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+ci:
+	./gitlab-ci.sh
---
+++ gitlab-ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# setup scone-cli as a git subtree
+#
+# (C) Christof Fetzer, 2017
+
+# ci
+   sudo SCONE_HOSTINSTALLER/install_patched_docker.sh
+# change user has not taken effect - run with sudo
+   sudo docker swarm init
+   sudo docker node update --label-add nodeno="1" $(sudo docker node ls -q)
+   sudo ./start-scope.sh
+   sleep 5
+   IP=`hostname -I | awk '{print $1;}'`
+   sudo docker service ls
+   sudo docker service ps SCOPE
+   sudo docker ps -a
+   wget --retry-connrefused --waitretry=1 --read-timeout=30 --timeout=15 -t 25 http://$IP:4040
+   sudo ./stop-scope.sh
